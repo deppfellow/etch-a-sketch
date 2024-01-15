b4133623062490f046839f63ddd62def7a8404d1
deppfellow <raid.rafif3@gmail.com>
2024-01-15 20:26:30 +0700

    Logic mouse hover turn to black

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const gridInput = prompt("Enter the grid dimension:")
 document.documentElement.style.setProperty('--columns', gridInput)
 
-// const gridInput = document.querySelector(".dim-input")
-
 function createGrid(gridDimension) {
     const gridContainer = document.querySelector('.main-container')
     for (let i = 0; i < gridDimension * gridDimension; i++) {
         const gridItem = document.createElement('div')
         gridItem.classList.add("grid-item")
-        gridContainer.appendChild(gridItem)
+        gridItem.addEventListener('mouseover', function (ev) {
+            ev.target.style.backgroundColor = "#000"
+        })
 
+        gridContainer.appendChild(gridItem)
     }
 }
+
+// gridInput.addEventListener("change", function () {
+//     let gridValue = parseInt(gridInput, 10) || 0
+//     document.documentElement.style.setProperty('--columns', gridValue)
+
+//     createGrid(gridValue)
+// })
 
 createGrid(gridInput)
